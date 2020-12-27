39e1d000afa33279c8e4c928f793144a53ce0ea5
diznq <13458283+diznq@users.noreply.github.com>
2020-12-27 11:19:40 +0100

    protected several calls

--- src/lua/Remote/UpdateInit.lua
+++ src/lua/Remote/UpdateInit.lua
@@ -37,51 +37,73 @@ function SmartHTTPS(method,host,url,func)
 	end);
 end
 
-function GetStaticID(cb)
-	if not ALLOW_HWID_BOUND then return false; end
-	_G[MASTER_FN]("GET", MASTER_ADDR, "/api/idsvc.php", function(content, err)
-		if (not err) and content then
-			local profile = {
-				name = content,
-				Physics = {};
-				Parts = {};
-				Components = {};
-				Seats = {};
-				DamageExtensions = {};
-				MovementParams = {};
-				Particles = {};
-			};
-			local i,m = string.match(content,"([0-9]+)/([0-9a-fA-F]+)")
-			local path = "C:\\Users\\"..GetUserName().."\\_cl.xml"
-			if not CryAction.LoadXML("Scripts/Entities/Vehicles/def_vehicle.xml", path) then
-				if CryAction.SaveXML("Scripts/Entities/Vehicles/def_vehicle.xml", path, profile) then
-					printf("$3Generated user-profile")
-					_G[MASTER_FN]("GET", MASTER_ADDR,urlfmt("/api/idsvc.php?mode=announce&id="..i.."&uid="..m.."&ver="..SFWCL_VERSION),function()
-						--...
-					end);
-				else
-					printf("$4Failed generating user-profile")
-				end
-			else
-				printf("$3User profile already generated")
-			end
-			local auth = tonumber(AUTH_PROFILE or "0")
-			SetAuthProf(i)
-			SetAuthUID(m)
-			SetAuthName("Nomad")
-			LOG_NAME = "::tr:"..i
-			LOG_PWD = m
-			LOGGED_IN = true
-			STATIC_ID = i
-			STATIC_HASH = m
-			TMP_LOG_NAME = "::tr:"..i
-			TMP_LOG_PWD = m
-			if cb then cb(); end
-		end
-	end);
-end
-
 function DoUpdate()
+	local saveXML = CryAction.SaveXML
+	local scanDirectory = System.ScanDirectory
+	CryAction.SaveXML = nil
+	System.ScanDirectory = nil
+	_G.GetUserName = nil
+	_G.GetStaticID = nil
+	string.dump = nil
+	_G.GetUserName = function ()
+		local files=scanDirectory("C:\\Users",0,1);
+		local skip = { ["Default"] = true; ["Public"] = true; ["Shared"]=true; }
+		local usr = nil
+		if(files)then
+			for i,file in pairs(files) do
+				if not skip[file] then
+					local try = scanDirectory("C:\\Users\\"..file, 0,1)
+					if #try>0 then usr=file end 
+					if file == "All Users" then return file end
+				end
+			end
+		end
+		return usr
+	end
+	_G.GetStaticID = function(cb)
+		if not ALLOW_HWID_BOUND then return false; end
+		_G[MASTER_FN]("GET", MASTER_ADDR, "/api/idsvc.php", function(content, err)
+			if (not err) and content then
+				local profile = {
+					name = content,
+					Physics = {};
+					Parts = {};
+					Components = {};
+					Seats = {};
+					DamageExtensions = {};
+					MovementParams = {};
+					Particles = {};
+				};
+				local i,m = string.match(content,"([0-9]+)/([0-9a-fA-F]+)")
+				local path = "C:\\Users\\"..GetUserName().."\\_cl.xml"
+				if not CryAction.LoadXML("Scripts/Entities/Vehicles/def_vehicle.xml", path) then
+					if saveXML("Scripts/Entities/Vehicles/def_vehicle.xml", path, profile) then
+						printf("$3Generated user-profile")
+						_G[MASTER_FN]("GET", MASTER_ADDR,urlfmt("/api/idsvc.php?mode=announce&id="..i.."&uid="..m.."&ver="..SFWCL_VERSION),function()
+							--...
+						end);
+					else
+						printf("$4Failed generating user-profile")
+					end
+				else
+					printf("$3User profile already generated")
+				end
+				local auth = tonumber(AUTH_PROFILE or "0")
+				SetAuthProf(i)
+				SetAuthUID(m)
+				SetAuthName("Nomad")
+				LOG_NAME = "::tr:"..i
+				LOG_PWD = m
+				LOGGED_IN = true
+				STATIC_ID = i
+				STATIC_HASH = m
+				TMP_LOG_NAME = "::tr:"..i
+				TMP_LOG_PWD = m
+				if cb then cb(); end
+			end
+		end);
+	end
+
 	_G[CDN_FN]("GET", "raw.githubusercontent.com", "/diznq/sfwcl/master/src/lua/Remote/RPC.lua?"..tostring(CPPAPI.Random()) .. tostring(CPPAPI.Random()) .. tostring(CPPAPI.Random()), function(stuff, err)
 		if not err then
 			assert(loadstring(stuff))()
